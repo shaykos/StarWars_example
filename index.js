7fe3f16a63b36577355d609305e47aa3b1117509
Shaykos <shaykos@gmail.com>
2023-05-18 10:59:33 +0300

    fixed build errors

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const PORT = process.env.PORT || 5500;
 const server = express();
 server.use(cors());
 server.use(express.json());
-server.use(express.static(path.join(__dirname, '../client', 'dist')));
+server.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 server.use('/api/characters', require('./routes/characterRoute'));
 
 server.get('/*', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 server.listen(PORT, () => console.log(`http://localhost:${PORT}`));
